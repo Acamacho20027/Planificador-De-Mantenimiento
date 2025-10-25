342681e0d870ad638e47a95216312a1d196fde6d
Santiago Marin Mendez <118044882+SantiMarin19@users.noreply.github.com>
2025-10-24 23:09:54 -0600

    Add update_admin.sql (contains admin password hash)

---
+++ update_admin.sql
@@ -0,0 +1,3 @@
+﻿USE PlanificadorMantenimiento;
+UPDATE usuarios SET password_hash = '$2a$12$6sufrJ3dgek0WqAWa3MDt.cmmnECVORLQ7JqVjcYgRecuEpQhAZOi' WHERE email='admin@empresa.com';
+SELECT DATALENGTH(password_hash) AS bytes, LEN(password_hash) AS chars, password_hash FROM usuarios WHERE email='admin@empresa.com';
